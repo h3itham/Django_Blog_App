dd660f4cbc0d01b1be0878748d4b1d607984cf03
h3itham <eng.haithamelabd@gmail.com>
2023-07-23 20:48:00 +0300

    adding delete post function

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import Home, Post_page, New, Edit
+from .views import Home, Post_page, New, Edit, Delete
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('post/<int:pk>/', Post_page.as_view(), name='post_page'),
     path('newpost/', New.as_view(), name='newpost'),
-    path('post/<int:pk>/edit/', Edit.as_view(), name='post_edit')
+    path('post/<int:pk>/edit/', Edit.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', Delete.as_view(), name='post_delete'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, TemplateView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, TemplateView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -24,3 +25,9 @@ class Edit(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+
+class Delete(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
