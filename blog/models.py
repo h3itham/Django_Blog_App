cd1a42017a242c5aca144855dc081f5cdcca8df9
h3itham <eng.haithamelabd@gmail.com>
2023-07-23 20:11:45 +0300

    solving page_detail problem

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
-class Post(models.Model): 
-    title = models.CharField(max_length = 200)
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE,)
     body = models.TextField()
 
-    def __str__(self): 
+    def __str__(self):
         return self.title
+
+    def get_absolute_url(self):  # new
+        return reverse('post_page', args=[str(self.id)])
