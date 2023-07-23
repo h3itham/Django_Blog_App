a9d9d69c34dcaee93445469de7404889da14b36e
h3itham <eng.haithamelabd@gmail.com>
2023-07-23 15:39:01 +0300

    add one post page fuctionality

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path 
-from .views import Home
+from django.urls import path
+from .views import Home, Post_page
 
 urlpatterns = [
-    path('', Home.as_view(), name = 'home')
+    path('', Home.as_view(), name='home'),
+    path('post/<int:pk>/', Post_page.as_view(), name='post_page')
 ]
